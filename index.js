fa4fb0d64f55b228c5d152c768a3c955a9317909
Sulav gautam <gautamsulav000@gmail.com>
2025-01-18 18:32:30 +0545

    Add edit Title option

--- index.js
+++ index.js
@@ -19,12 +19,23 @@ app.get("/", function (req, res) {
     res.render("index", { files: files });
   });
 });
+
+app.get("/edit/:filename", function (req, res) {
+  res.render("edit", { filename: req.params.filename });
+});
+app.post("/edit", function (req, res) {
+  fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err){
+    res.redirect("/");
+  });
+
+});
+
 app.get("/files/:filename", function (req, res) {
   fs.readFile(
     `./files/${req.params.filename}`,
     "utf-8",
     function (err, filedata) {
-      res.render("show", {filename: req.params.filename, filedata: filedata});
+      res.render("show", { filename: req.params.filename, filedata: filedata });
     }
   );
 });
